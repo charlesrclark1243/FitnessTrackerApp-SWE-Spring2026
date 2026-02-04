1e0707b26c09806b5a8e2bfd28266838f3eadad7
charlesrclark1243 <charlie.c1243@gmail.com>
2026-02-04 15:55:19 -0500

    generated backend skeleton

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/charlesrclark1243/FitnessTrackerApp-SWE-Spring2026/backend
+
+go 1.25.6
---
+++ backend/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Fitness Tracker API")
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "OK")
+	})
+
+	log.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
