02e6f1231f589f5488e856234386eae319481fdd
charlesrclark1243 <charlie.c1243@gmail.com>
2026-02-18 17:34:44 -0500

    Renamed register and login routes

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -3,19 +3,19 @@ package routes
 import (
 	"gorm.io/gorm"
 
-	"github.com/gin-gonic/gin"
 	"github.com/charlesrclark1243/FitnessTrackerApp-SWE-Spring2026/backend/handlers"
 	"github.com/charlesrclark1243/FitnessTrackerApp-SWE-Spring2026/backend/middleware"
+	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(router *gin.Engine, db *gorm.DB) {
 	api := router.Group("/api")
 	{
 		//public endpoints
-		api.POST("/register", func(c *gin.Context) {
+		api.POST("/auth/register/", func(c *gin.Context) {
 			handlers.Register(c, db)
 		})
-		api.POST("/login", func(c *gin.Context) {
+		api.POST("/auth/login", func(c *gin.Context) {
 			handlers.Login(c, db)
 		})
 
@@ -26,7 +26,7 @@ func SetupRoutes(router *gin.Engine, db *gorm.DB) {
 			// health Profile CRUD
 			protected.GET("/profile", handlers.GetProfile)
 			protected.PUT("/profile", handlers.UpdateProfile)
-			
+
 			// stats calculation
 			protected.GET("/profile/stats", handlers.GetStats)
 		}
