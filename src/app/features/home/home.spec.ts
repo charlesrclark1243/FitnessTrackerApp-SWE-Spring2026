de87f37658c35b726b67ef5487285e1601ec114b
Ahmed-Rageeb <143149775+Ahmed-Rageeb@users.noreply.github.com>
2026-03-10 06:44:19 -0400

    Add files via upload

--- src/app/features/home/home.spec.ts
+++ src/app/features/home/home.spec.ts
@@ -1,7 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { provideRouter } from '@angular/router';
 
 import { HomeComponent } from './home';
 
@@ -11,8 +8,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent],
-      providers: [provideHttpClient(), provideHttpClientTesting(), provideRouter([])]
+      imports: [HomeComponent]
     })
     .compileComponents();
 
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { AuthService } from '../../core/services/auth';
+import { WaterIntakeComponent } from './components/water-intake/water-intake';
+import { WaterDisplayComponent } from './components/water-display/water-display';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [
+    CommonModule, 
+    MatCardModule,
+    WaterIntakeComponent,
+    WaterDisplayComponent
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
