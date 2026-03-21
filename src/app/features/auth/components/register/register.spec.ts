961e56dd663f1530d5d69cf978093735efec4af4
Ahmed-Rageeb <143149775+Ahmed-Rageeb@users.noreply.github.com>
2026-03-20 23:34:19 -0400

    Update register.spec.ts

--- src/app/features/auth/components/register/register.spec.ts
+++ src/app/features/auth/components/register/register.spec.ts
@@ -1,9 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { provideRouter } from '@angular/router';
-
 import { RegisterComponent } from './register';
+import { provideHttpClient } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -12,16 +10,61 @@ describe('RegisterComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RegisterComponent],
-      providers: [provideHttpClient(), provideHttpClientTesting(), provideRouter([])]
-    })
-    .compileComponents();
+      providers: [
+        provideHttpClient(),
+        provideRouter([])
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the register component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a registration form', () => {
+    expect(component.registerForm).toBeTruthy();
+  });
+
+  it('should have username field', () => {
+    expect(component.registerForm.get('username')).toBeTruthy();
+  });
+
+  it('should have password field', () => {
+    expect(component.registerForm.get('password')).toBeTruthy();
+  });
+
+  it('should have confirmPassword field', () => {
+    expect(component.registerForm.get('confirmPassword')).toBeTruthy();
+  });
+
+  it('should have height field', () => {
+    expect(component.registerForm.get('height')).toBeTruthy();
+  });
+
+  it('should have weight field', () => {
+    expect(component.registerForm.get('weight')).toBeTruthy();
+  });
+
+  it('should have dateOfBirth field', () => {
+    expect(component.registerForm.get('dateOfBirth')).toBeTruthy();
+  });
+
+  it('should have sex field', () => {
+    expect(component.registerForm.get('sex')).toBeTruthy();
+  });
+
+  it('should mark empty form as invalid', () => {
+    expect(component.registerForm.valid).toBeFalsy();
+  });
+
+  it('should validate password mismatch', () => {
+    component.registerForm.controls['password'].setValue('password123');
+    component.registerForm.controls['confirmPassword'].setValue('different');
+    
+    expect(component.registerForm.controls['confirmPassword'].errors?.['passwordMismatch']).toBeTruthy();
   });
 });
